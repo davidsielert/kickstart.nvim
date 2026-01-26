7eb0c84c48c956040e0ee6067edaaa5b4e008e1a
David Sielert <456592+davidsielert@users.noreply.github.com>
2026-01-25 20:15:19 -0800

    updates for recent version

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -150,7 +150,8 @@ return {
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
-        tsserver = {},
+        ts_ls = {},
+        -- -tsserver = {},
         terraformls = {},
         bashls = {},
         html = {},
