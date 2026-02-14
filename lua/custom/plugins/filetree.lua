f796059a2aebca28073859aa911bb40d9495e454
David Sielert <456592+davidsielert@users.noreply.github.com>
2026-02-14 12:36:55 -0800

    add file tree

---
+++ lua/custom/plugins/filetree.lua
@@ -0,0 +1,13 @@
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+  },
+  config = function()
+    require('neo-tree').setup {}
+  end,
+  vim.keymap.set('n', '<leader>t', '<cmd>Neotree toggle<CR>', { desc = 'Toggle Neo-tree' }),
+}
